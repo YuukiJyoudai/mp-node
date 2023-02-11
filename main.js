e0d743642e5ef071943c13659e6e84fda8573d63
YuukiJyoudai <1374341727@qq.com>
2023-02-11 16:49:19 +0800

    feat(test): trst

--- main.js
+++ main.js
@@ -1,27 +1,47 @@
 const Koa = require('koa')
 const encryption = require('./util')
 const config = require('./config')
+const xmlParser = require('koa-xml-body')
 const app = new Koa()
 const {port, token} = config
 
+app.use(xmlParser())
 app.use(async (ctx, next) => {
     const {method} = ctx
-    console.log(ctx)
-    // let { signature = '', timestamp = '', nonce = '', echostr = '' } = ctx.query
-    // // 将token、timestamp、nouce 进行字典排序
-    // let str = [token, timestamp, nonce].sort().join('')
-    // // 将3个参数字符串拼接成字符串进行sha1加密
-    // const shaStr = encryption.sha1(str)
-    // if (method === 'GET') {
-    //     if (shaStr === signature) {
-    //         ctx.body = echostr
-    //     } else {
-    //         ctx.body = '服务器验证失败'
-    //     }
-    // } else if (method === 'POST') {
-    //     console.dir(`ctx.path--${ctx.pah}`)
-    //     console.dir(`ctx.query--${ctx.query}`)
-    // }
+    let { signature = '', timestamp = '', nonce = '', echostr = '' } = ctx.query
+    // 将token、timestamp、nouce 进行字典排序
+    let str = [token, timestamp, nonce].sort().join('')
+    // 将3个参数字符串拼接成字符串进行sha1加密
+    const shaStr = encryption.sha1(str)
+    if (method === 'GET') {
+        if (shaStr === signature) {
+            // ctx.body = echostr
+            ctx.body = `
+                <xml>
+                <CreateTime>${new Date().getTime()}</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA[get测试]]></Content>
+                </xml>
+            `
+        } else {
+            ctx.body = '服务器验证失败'
+        }
+    } else if (method === 'POST') {
+        // 打印用户当前输入的内容是什么
+        const userText = ctx.request.body && ctx.request.body.xml && ctx.request.body.xml.MsgType && ctx.request.body.xml.MsgType[0]
+        console.log('用户发送了如下信息', userText)
+
+        // 这么简单？？？直接返回
+        // <ToUserName><![CDATA[${message.FromUserName}]]></ToUserName>
+        // <FromUserName><![CDATA[${message.ToUserName}]]></FromUserName>
+        ctx.body = `
+            <xml>
+            <CreateTime>${new Date().getTime()}</CreateTime>
+            <MsgType><![CDATA[text]]></MsgType>
+            <Content><![CDATA[欢迎来到我的公众号]]></Content>
+            </xml>
+        `
+    }
 })
 
 
---
+++ test.js
@@ -0,0 +1,73 @@
+{
+    request: {
+      method: 'POST',
+      url: '/?signature=23e05ecdc782c94ccc401577261d32f9fbddeee9&timestamp=1676101683&nonce=2080301931&openid=oIkBV6Ck3LOd5yR_mkQMegtxC6XY',
+      header: {
+        'user-agent': 'Mozilla/4.0',
+        accept: '*/*',
+        host: 'www.passerby.cc',
+        pragma: 'no-cache',
+        'content-length': '272',
+        'content-type': 'text/xml'
+      }
+    },
+    response: {
+      status: 404,
+      message: 'Not Found',
+      header: [Object: null prototype] {}
+    },
+    app: { subdomainOffset: 2, proxy: false, env: 'development' },
+    originalUrl: '/?signature=23e05ecdc782c94ccc401577261d32f9fbddeee9&timestamp=1676101683&nonce=2080301931&openid=oIkBV6Ck3LOd5yR_mkQMegtxC6XY',
+    req: '<original node req>',
+    res: '<original node res>',
+    socket: '<original node socket>'
+  }
+  {
+    request: {
+      method: 'POST',
+      url: '/?signature=23e05ecdc782c94ccc401577261d32f9fbddeee9&timestamp=1676101683&nonce=2080301931&openid=oIkBV6Ck3LOd5yR_mkQMegtxC6XY',
+      header: {
+        'user-agent': 'Mozilla/4.0',
+        accept: '*/*',
+        host: 'www.passerby.cc',
+        pragma: 'no-cache',
+        'content-length': '272',
+        'content-type': 'text/xml'
+      }
+    },
+    response: {
+      status: 404,
+      message: 'Not Found',
+      header: [Object: null prototype] {}
+    },
+    app: { subdomainOffset: 2, proxy: false, env: 'development' },
+    originalUrl: '/?signature=23e05ecdc782c94ccc401577261d32f9fbddeee9&timestamp=1676101683&nonce=2080301931&openid=oIkBV6Ck3LOd5yR_mkQMegtxC6XY',
+    req: '<original node req>',
+    res: '<original node res>',
+    socket: '<original node socket>'
+  }
+  {
+    request: {
+      method: 'POST',
+      url: '/?signature=23e05ecdc782c94ccc401577261d32f9fbddeee9&timestamp=1676101683&nonce=2080301931&openid=oIkBV6Ck3LOd5yR_mkQMegtxC6XY',
+      header: {
+        'user-agent': 'Mozilla/4.0',
+        accept: '*/*',
+        host: 'www.passerby.cc',
+        pragma: 'no-cache',
+        'content-length': '272',
+        'content-type': 'text/xml'
+      }
+    },
+    response: {
+      status: 404,
+      message: 'Not Found',
+      header: [Object: null prototype] {}
+    },
+    app: { subdomainOffset: 2, proxy: false, env: 'development' },
+    originalUrl: '/?signature=23e05ecdc782c94ccc401577261d32f9fbddeee9&timestamp=1676101683&nonce=2080301931&openid=oIkBV6Ck3LOd5yR_mkQMegtxC6XY',
+    req: '<original node req>',
+    res: '<original node res>',
+    socket: '<original node socket>'
+  }
+  
